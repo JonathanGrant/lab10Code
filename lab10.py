060aa5534eca2ed10e8b590beadb7aab4c212add
Jonathan Grant <jonathanallengrant@gmail.com>
2016-03-31 20:34:50 -0700

    First commit

---
+++ lab10.py
@@ -0,0 +1,114 @@
+import create2
+import lab10_map
+import math
+from particle_filter import ParticleFilter as PF
+from odometry import Odometry
+
+class Run:
+    def __init__(self, factory):
+        """Constructor.
+
+        Args:
+            factory (factory.FactoryCreate)
+        """
+        self.create = factory.create_create()
+        self.time = factory.create_time_helper()
+        self.servo = factory.create_servo()
+        self.sonar = factory.create_sonar()
+        # Add the IP-address of your computer here if you run on the robot
+        self.virtual_create = factory.create_virtual_create()
+        self.map = lab10_map.Map("lab10.map")
+        self.odometry = Odometry()
+        
+    def showParticles(self, particles):
+        self.virtual_create.set_point_cloud(particles)
+        
+    def visualizePose(self,x,y,z,theta):
+        self.virtual_create.set_pose((x, y, z), theta)
+
+    def run(self):
+        self.create.start()
+        self.create.safe()
+        
+        # request sensors
+        self.create.start_stream([
+            create2.Sensor.LeftEncoderCounts,
+            create2.Sensor.RightEncoderCounts,
+        ])
+        
+        # This is an example on how to visualize the pose of our estimated position
+        # where our estimate is that the robot is at (x,y,z)=(0.5,0.5,0.1) with heading pi
+        self.visualizePose(0.5, 0.5, 0.1, math.pi)
+
+        # This is an example on how to show particles
+        # the format is x,y,z,theta,x,y,z,theta,...
+        data = [0.5, 0.5, 0.1, math.pi/2, 1.5, 1, 0.1, 0]
+        self.showParticles(data)
+
+        # This is an example on how to estimate the distance to a wall for the given
+        # map, assuming the robot is at (0, 0) and has heading math.pi
+        print(self.map.closest_distance((0.5,0.5), math.pi))
+        
+        self.state = self.create.update()
+        self.odometry.update(self.state.leftEncoderCounts, self.state.rightEncoderCounts)
+        
+        #This is an example on how to use PF
+        variances = [0.01,0.01,0.3]
+        self.pf = PF([self.map.bottom_left[0], self.map.top_right[0]],[self.map.bottom_left[1], self.map.top_right[1]],[0,2*math.pi], variances, self.map)
+        print("Drawing particles")
+        for particle in self.pf.getParticles():
+            self.showParticles([particle.x,particle.y,0.1,particle.theta])
+        
+        # This is an example on how to detect that a button was pressed in V-REP
+        while True:
+            self.create.drive_direct(0,0)
+            
+            self.state = self.create.update()
+            self.odometry.update(self.state.leftEncoderCounts, self.state.rightEncoderCounts)
+            prevX = self.odometry.x
+            prevY = self.odometry.y
+            prevTheta = self.odometry.theta
+            
+            b = self.virtual_create.get_last_button()
+            if b == self.virtual_create.Button.MoveForward:
+                print("Forward pressed!")
+                self.create.drive_direct(50,50)
+                self.time.sleep(1)
+                self.create.drive_direct(0,0)
+                self.state = self.create.update()
+                self.odometry.update(self.state.leftEncoderCounts, self.state.rightEncoderCounts)
+                self.pf.MoveForward()
+                print("Drawing particles")
+                for particle in self.pf.getParticles():
+                    self.showParticles([particle.x,particle.y,0.1,particle.theta])
+            elif b == self.virtual_create.Button.TurnLeft:
+                print("Turn Left pressed!")
+                self.create.drive_direct(50,-50)
+                self.time.sleep(1)
+                self.create.drive_direct(0,0)
+                self.state = self.create.update()
+                self.odometry.update(self.state.leftEncoderCounts, self.state.rightEncoderCounts)
+                self.pf.TurnLeft()
+                print("Drawing particles")
+                for particle in self.pf.getParticles():
+                    self.showParticles([particle.x,particle.y,0.1,particle.theta])
+            elif b == self.virtual_create.Button.TurnRight:
+                print("Turn Right pressed!")
+                self.create.drive_direct(-50,50)
+                self.time.sleep(1)
+                self.create.drive_direct(0,0)
+                self.state = self.create.update()
+                self.odometry.update(self.state.leftEncoderCounts, self.state.rightEncoderCounts)
+                self.pf.TurnRight()
+                print("Drawing particles")
+                for particle in self.pf.getParticles():
+                    self.showParticles([particle.x,particle.y,0.1,particle.theta])
+            elif b == self.virtual_create.Button.Sense:
+                print("Sense pressed!")
+                self.pf.Sensing(self.sonar.get_distance())
+                print("Drawing particles")
+                for particle in self.pf.getParticles():
+                    self.showParticles([particle.x,particle.y,0.1,particle.theta])
+
+            self.time.sleep(0.01)
+            
---
+++ particle_filter.py
@@ -0,0 +1,82 @@
+import math
+import numpy as np
+import scipy.stats
+
+class Particle:
+    def __init__(self, x, y, theta, variances):
+        #Fill in Location
+        self.x = x
+        self.y = y
+        self.theta = theta
+        self.varX = variances[0]
+        self.varY = variances[1]
+        self.varTheta = variances[2]
+
+class ParticleFilter:
+    def __init__(self, boundsX, boundsY, boundsTheta, variances, theMap):
+        """Constructor.
+        """
+        #Here I should create a whole bunch of particles, right?
+        self.theMap = theMap
+        self.particles = self.createUniformParticles(boundsX, boundsY, boundsTheta, variances)
+        print("Particles are done being created.")
+        
+    
+    
+    def getParticles(self):
+        return self.particles
+    
+    def MoveForward(self):
+        self.Movement(0, 0.1, 0.052458669781640876, 0.01)
+    
+    def TurnLeft(self):
+        self.Movement(0.4464567640990713, 0.1, 0, 0.01)
+            
+    def TurnRight(self):
+        self.Movement(-0.4464567640990713, 0.1, 0, 0.01)
+    
+    def Movement(self, theta, varTheta, dist, varDist):
+        print("Some shit moved brah")
+        #Move particles
+        for particle in self.particles:
+            thetaPrime = theta + self.doNormal(0, varTheta)
+            distPrime = dist + self.doNormal(0, varDist)
+            particle.theta += thetaPrime
+            particle.x += distPrime * math.cos(particle.theta)
+            particle.y += distPrime * math.sin(particle.theta)
+        print("Particles are done moving.")
+    
+    def Sensing(self, reading):
+        prob = []
+        #Find probability for each particle
+        for particle in self.particles:
+            dist = self.theMap.closest_distance((particle.x, particle.y), particle.theta)
+            if dist:
+                prob.append(scipy.stats.norm(reading, 0.1).pdf(dist))
+        #Based on that, resample from that data
+        newParticles = []
+        for i in range(0,100):
+            party = np.random.choice(self.particles,1,prob)[0]
+            newParticles.append(self.createUniformParticle([party.x-0.1,party.x+0.1], [party.y-0.1,party.y+0.1], [party.theta-0.3,party.theta+0.3],[party.varX,party.varY,party.varTheta]))
+        self.particles = newParticles
+    
+    def Estimation(self):
+        pass
+    
+    def doNormal(self, mean, variance):
+        return np.random.normal(mean, variance)
+    
+    def doUniform(self, minnie, maxie):
+        return np.random.uniform(minnie,maxie)
+    
+    def createUniformParticle(self, boundsX, boundsY, boundsTheta, variances):
+        return Particle(self.doUniform(boundsX[0], boundsX[1]), self.doUniform(boundsY[0], boundsY[1]), self.doUniform(boundsTheta[0], boundsTheta[1]), variances)
+    
+    def createUniformParticles(self, boundsX, boundsY, boundsTheta, variances):
+        Particles = []
+        for i in range(0, 100):
+            Particles.append(Particle(self.doUniform(boundsX[0], boundsX[1]), self.doUniform(boundsY[0], boundsY[1]), self.doUniform(boundsTheta[0], boundsTheta[1]), variances))
+        return Particles
+    
+    def moveNormalParticle(self, particle, dX, dY, dTheta):
+        return [self.doNormal(dX, particle.varX), self.doNormal(dY, particle.varY), self.doNormal(dTheta, particle.varTheta)]
